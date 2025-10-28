cfa4c3f3dfcb1ed2fa90d3ce494058bf26960ad3
Muhammad Aliff Mohamed Anuar <96939727+Moon1100@users.noreply.github.com>
2025-10-28 14:50:02 +0800

    refactor: Clean up app layout by commenting out unused Alpine.js script and improving success message display

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-<script src="//unpkg.com/alpinejs" defer></script>
+    {{-- <script src="//unpkg.com/alpinejs" defer></script> --}}
 
 
     <title>{{ config('app.name', 'Plant Management') }}</title>
 
-    <icon rel="icon" href="{{ asset("favicon.ico") }}" />
+    <icon rel="icon" href="{{ asset('favicon.ico') }}" />
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -101,7 +101,7 @@
                                 </form>
                             </div>
                         </div>
-                        @else
+                    @else
                         <!-- Google Sign-In -->
                         {{-- <div id="g_id_onload" data-client_id="{{ config('services.google.client_id') }}"
                             data-login_uri="{{ Route::has('auth.google.callback') ? route('auth.google.callback') : url('/auth/google/callback') }}"
@@ -181,33 +181,23 @@
     </nav>
 
     <!-- Flash Messages -->
-@if (session('success'))
-    <div class="flex justify-center mt-4">
-        <div
-            x-data="{ show: true }"
-            x-show="show"
-            x-transition
-            class="relative max-w-4xl w-full bg-green-50 dark:bg-green-900 border border-green-200 dark:border-green-800
-                   text-green-800 dark:text-green-100 px-4 py-3 rounded shadow-md"
-        >
-            <span class="block text-center sm:inline">{{ session('success') }}</span>
+    @if (session('success'))
+        <div class="flex justify-center mt-4">
+            <div x-data="{ show: true }" x-show="show" x-transition
+                class="relative max-w-4xl w-full bg-green-50 dark:bg-green-900 border border-green-200 dark:border-green-800
+                   text-green-800 dark:text-green-100 px-4 py-3 rounded shadow-md">
+                <span class="block text-center sm:inline">{{ session('success') }}</span>
 
-            <button
-                @click="show = false"
-                class="absolute top-0 bottom-0 right-0 px-4 py-3 focus:outline-none"
-            >
-                <svg
-                    class="fill-current h-6 w-6 text-green-500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                >
-                    <path
-                        d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                </svg>
-            </button>
+                <button @click="show = false" class="absolute top-0 bottom-0 right-0 px-4 py-3 focus:outline-none">
+                    <svg class="fill-current h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20">
+                        <path
+                            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                    </svg>
+                </button>
+            </div>
         </div>
-    </div>
-@endif
+    @endif
 
 
     <!-- Page Content -->
