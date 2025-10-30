4f85423bc1104002d6a5c6bfb7cb07dabea15b09
Muhammad Aliff Mohamed Anuar <96939727+Moon1100@users.noreply.github.com>
2025-10-30 10:12:50 +0800

    refactor: Comment out unused icon tag in layout for cleaner code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <title>{{ config('app.name', 'Plant Management') }}</title>
 
-    <icon rel="icon" href="{{ asset('favicon.ico') }}" />
+    {{-- <icon rel="icon" href="{{ asset('favicon.ico') }}" /> --}}
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
