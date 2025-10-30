f6610b2ff0b82851acdf0447f5662a047f1d9cf1
Muhammad Aliff Mohamed Anuar <96939727+Moon1100@users.noreply.github.com>
2025-10-30 10:12:22 +0800

    feat: Add multiple favicon and apple-touch-icon images for improved branding and compatibility

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- <script src="//unpkg.com/alpinejs" defer></script> --}}
+<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
 
 
     <title>{{ config('app.name', 'Plant Management') }}</title>
